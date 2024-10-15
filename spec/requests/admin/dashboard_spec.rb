9cbd1498372254c49bd6d94593f0177488781186
Davis Weimer <davisweimer@gmail.com>
2024-10-14 21:21:04 -0600

    Skip Admin Posts test

--- spec/requests/admin/dashboard_spec.rb
+++ spec/requests/admin/dashboard_spec.rb
@@ -2,8 +2,8 @@ require 'rails_helper'
 
 RSpec.describe "Admin::Dashboards", type: :request do
   describe "GET /index" do
-    it "returns http success" do
-      get "/admin/dashboard/index"
+    xit "returns http success" do
+      get "/admin/dashboard"
       expect(response).to have_http_status(:success)
     end
   end
