175ae7beaa205b6743ae59c933c836a932dd4e74
Davis Weimer <davisweimer@gmail.com>
2024-10-15 19:54:24 -0600

    Add and test basic Admin Dashboard

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,5 +1,10 @@
 class Admin::DashboardController < ApplicationController
   before_action :authenticate_user!
   def index
+    @total_posts = Post.count
+    @total_topcis = Category.count
+    @total_tags = Tag.count
+    @draft_posts = Post.where(status: 'Draft').count
+    @posts_by_category = Category.joins(:posts).group('categories.name').count
   end
 end
