940971ed8c61779c91a4260c3bffd6789e3374db
Davis Weimer <davisweimer@gmail.com>
2024-10-16 14:54:59 -0600

    Add basic Post request tests

--- spec/requests/admin/posts_spec.rb
+++ spec/requests/admin/posts_spec.rb
@@ -1,9 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe "Admin::Posts", type: :request do
+  let(:user) { User.create(email: 'blogboy@example.com', password: 'password') }
+  let(:post) { Post.create(title: 'First Post', content: 'Content', status: 'publish', user: user) }
+  before do
+    sign_in user
+  end
+
   describe "GET /index" do
     it "returns http success" do
-      get "/admin/posts/index"
+      get admin_posts_path
       expect(response).to have_http_status(:success)
     end
   end
@@ -17,9 +23,8 @@ RSpec.describe "Admin::Posts", type: :request do
 
   describe "GET /edit" do
     it "returns http success" do
-      get "/admin/posts/edit"
+      get "/admin/posts/#{post.id}/edit"
       expect(response).to have_http_status(:success)
     end
   end
-
 end
