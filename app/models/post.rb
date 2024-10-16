e2d16a53fc1ddc6c0d30779d385cfa11fc5627bf
Davis Weimer <davisweimer@gmail.com>
2024-10-15 19:31:57 -0600

    Add joins table to complete associations

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,10 @@
 class Post < ApplicationRecord
   belongs_to :user
+  has_and_belongs_to_many :categories
+  has_and_belongs_to_many :tags
+
+  has_rich_text :have_content
+
+  extend FriendlyId
+  friendly_id :title, use: :slugged
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_10_16_012708) do
+ActiveRecord::Schema[7.1].define(version: 2024_10_16_013047) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -18,6 +18,11 @@ ActiveRecord::Schema[7.1].define(version: 2024_10_16_012708) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "categories_posts", id: false, force: :cascade do |t|
+    t.bigint "post_id", null: false
+    t.bigint "category_id", null: false
   end
 
   create_table "posts", force: :cascade do |t|
@@ -29,6 +34,11 @@ ActiveRecord::Schema[7.1].define(version: 2024_10_16_012708) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_posts_on_user_id"
+  end
+
+  create_table "posts_tags", id: false, force: :cascade do |t|
+    t.bigint "post_id", null: false
+    t.bigint "tag_id", null: false
   end
 
   create_table "tags", force: :cascade do |t|
