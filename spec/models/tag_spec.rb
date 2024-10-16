45e38d5762420e0cb9cd466c0de7c93b048df61f
Davis Weimer <davisweimer@gmail.com>
2024-10-15 20:35:37 -0600

    Add basic tests for Tag model

--- spec/models/tag_spec.rb
+++ spec/models/tag_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Tag, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "Associations" do
+    it { should have_and_belong_to_many(:posts) }
+  end
+
+  describe "Validations" do
+    it { should validate_presence_of(:name) }
+  end
 end
