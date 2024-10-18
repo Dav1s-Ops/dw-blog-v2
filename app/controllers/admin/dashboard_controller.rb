089e0476045dc6d7c66045cd6d6525d87f6326f1
Davis Weimer <davisweimer@gmail.com>
2024-10-17 21:11:43 -0600

    Adjust variable name to Categories

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -2,7 +2,7 @@ class Admin::DashboardController < ApplicationController
   before_action :authenticate_user!
   def index
     @total_posts = Post.count
-    @total_topics = Category.count
+    @total_categories = Category.count
     @total_tags = Tag.count
     @draft_posts = Post.where(status: 'Draft').count
     @posts_by_category = Category.joins(:posts).group('categories.name').count
