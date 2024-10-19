2383ddc847bca77856271164920b44bc6989a7bb
Davis Weimer <davisweimer@gmail.com>
2024-10-18 21:52:36 -0600

    Add Modal controller

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -2,6 +2,3 @@
 import { application } from "controllers/application"
 import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
 eagerLoadControllersFrom("controllers", application)
-
-import FlashController from "./flash_controller.js"
-application.register("flash", FlashController)
---
+++ app/javascript/controllers/modal_controller.js
@@ -0,0 +1,20 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  static targets = ["modal"]
+
+  connect() {
+    console.log("Modal controller connected")
+  }
+
+  open(event) {
+    event.preventDefault()
+    console.log("Open method called")
+    this.modalTarget.classList.remove('hidden')
+  }
+
+  close() {
+    console.log("Close method called")
+    this.modalTarget.classList.add('hidden')
+  }
+}
