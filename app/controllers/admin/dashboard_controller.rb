fbc25f262856be3f40a4980e58c61c578034b86b
Davis Weimer <davisweimer@gmail.com>
2024-10-20 16:38:14 -0600

    Create Admin::ApplicationController

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,5 +1,4 @@
-class Admin::DashboardController < ApplicationController
-  before_action :authenticate_user!
+class Admin::DashboardController < Admin::ApplicationController
   def index
     @total_posts = Post.count
     @total_categories = Category.count
--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -1,15 +1,48 @@
-class Admin::PostsController < ApplicationController
-  before_action :authenticate_user!
+class Admin::PostsController < Admin::ApplicationController
+  before_action :set_post, only: [:edit, :update, :destroy]
   def index
     @pagy, @posts = pagy(Post.order(created_at: :desc), limit: 10)
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to admin_posts_path, notice: 'Post created.'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @post.update(post_params)
+      redirect_to admin_posts_path, notice: 'Post updated.'
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @post.destroy
+    redirect_to admin_posts_path, notice: 'Post deleted.'
   end
 
   def show
   end
 
-  def new
+  def edit
   end
 
-  def edit
+  private
+
+  def set_post
+    @post = Post.friendly.find(params[:id])
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :content, :status, :slug)
   end
 end
