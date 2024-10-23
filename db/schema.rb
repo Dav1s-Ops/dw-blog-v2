9711cd80b2a6374424a2946525b73bc5b285584a
Davis Weimer <davisweimer@gmail.com>
2024-10-22 21:18:31 -0600

    Remove content column for ActionText content

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_10_20_221924) do
+ActiveRecord::Schema[7.1].define(version: 2024_10_23_030350) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -65,7 +65,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_10_20_221924) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
-    t.text "content"
     t.string "status"
     t.string "slug"
     t.bigint "user_id", null: false
